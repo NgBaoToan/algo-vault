15c42ec2daffc217eddeff1741869f882283db1e
NgBaoToan <nguyenbaotoan14092006@gmail.com>
2026-09-11 01:58:48 +0700

    update

--- a.cpp
+++ a.cpp
@@ -1,25 +1,41 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+const int MOD = 1e9 + 7;
+const int INF = 1e6 + 6;
+
+#define ll long long
+#define ld long double
+#define ar array
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define ft first
+#define sd second
+#define sz(x) (int)(x).size()
+
+void solve(){
+    ll x, y, k;
+    cin >> x >> y >> k;
+
+    ll D = y - x;
+    ll ans = 0;
+    for(ll i = 0; i < k; i++){
+        if(x + i > D){
+            ans += (k - i) * D;
+            break;
+        }
+        ans += D % (x + i);
+    }
+
+    cout << ans << '\n';
+}
+
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int n; cin >> n;
-    int a[n + 1][n + 1];
-
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
-            a[i][j] = (i ^ j);
-        }
-    }
-
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
-            cout << a[i][j] << ' ';
-        }
-        cout << '\n';
-    }
-
+    int t; cin >> t;
+    while(t--) solve();
     return 0;
 }
