91f94877b5977fc3ad53b89509e58cf529e98ccb
NgBaoToan <nguyenbaotoan14092006@gmail.com>
2026-09-12 01:04:38 +0700

    add file

--- a.cpp
+++ a.cpp
@@ -15,27 +15,28 @@ const int INF = 1e6 + 6;
 #define sz(x) (int)(x).size()
 
 void solve(){
-    ll x, y, k;
-    cin >> x >> y >> k;
-
-    ll D = y - x;
-    ll ans = 0;
-    for(ll i = 0; i < k; i++){
-        if(x + i > D){
-            ans += (k - i) * D;
-            break;
+    string s; cin >> s;
+    int n = s.size();
+    s = " " + s;
+    int q = 0;
+    int qa = 0;
+    int qaq = 0;
+    for(int i = 1; i <= n; i++){
+        if(s[i] == 'Q'){
+            qaq += qa;
+            q++;
+        }else if(s[i] == 'A'){
+            qa += q;
         }
-        ans += D % (x + i);
     }
 
-    cout << ans << '\n';
+    cout << qaq << '\n';
 }
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int t; cin >> t;
-    while(t--) solve();
+    solve();
     return 0;
 }
