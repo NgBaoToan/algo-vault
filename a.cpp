e5363a4806d2d398f23d877c70fd65c13673bd11
NgBaoToan <nguyenbaotoan14092006@gmail.com>
2026-09-14 22:51:05 +0700

    add file a.cpp

--- a.cpp
+++ a.cpp
@@ -1,42 +1,29 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-const int MOD = 1e9 + 7;
-const int INF = 1e6 + 6;
-
 #define ll long long
-#define ld long double
-#define ar array
-#define all(x) (x).begin(), (x).end()
-#define rall(x) (x).rbegin(), (x).rend()
-#define pb push_back
-#define ft first
-#define sd second
-#define sz(x) (int)(x).size()
-
-void solve(){
-    string s; cin >> s;
-    int n = s.size();
-    s = " " + s;
-    int q = 0;
-    int qa = 0;
-    int qaq = 0;
-    for(int i = 1; i <= n; i++){
-        if(s[i] == 'Q'){
-            qaq += qa;
-            q++;
-        }else if(s[i] == 'A'){
-            qa += q;
-        }
-    }
-
-    cout << qaq << '\n';
-}
+#define arr array
 
 int main() {
     ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+    cin.tie(0);
+    cout.tie(0);
 
-    solve();
-    return 0;
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    int n; cin >> n;
+    for(int i = 0; i < n; i++){
+        int a, b; cin >> a >> b;
+        if(a != b){
+            cout << "Happy Alex\n";
+            return 0;
+        }
+    }
+
+    cout << "Poor Alex\n";
+
+    return 0;   
 }
